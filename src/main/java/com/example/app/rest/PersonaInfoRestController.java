e5c6cea4f4f5b9bd8de1901a01d7d53cef7d718c
José Huenul <joseantonio20059@hotmail.com>
2019-11-18 00:28:18 -0300

    se crea clases para errores y se agrega metodo POST para agregar data

--- src/main/java/com/example/app/rest/PersonaInfoRestController.java
+++ src/main/java/com/example/app/rest/PersonaInfoRestController.java
@@ -1,17 +1,23 @@
 package com.example.app.rest;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.app.dao.PersonalInfoDao;
 import com.example.app.entity.PersonaInfo;
+import com.example.app.error.ErrorMessage;
 
 @RestController
 @RequestMapping("/persona")
@@ -31,6 +37,25 @@ public class PersonaInfoRestController {
 		return (personalInfoOp.isPresent() ? ResponseEntity.ok(personalInfoOp.get()) : ResponseEntity.noContent().build());
 	}
 	
-	
+	@PostMapping("/add-personal-info")
+	public ResponseEntity<Object> addPersonalInfo(@RequestBody PersonaInfo pi){
+		
+		ResponseEntity<Object> responseEntity = null;
+		
+		try {
+			if(pi != null) {
+				if(pi.getId() != null) {
+					responseEntity = new ResponseEntity<>(new ErrorMessage(new Date(), new Throwable("Please, don't send field 'id' on Body")), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+				}else {
+					responseEntity = ResponseEntity.ok(personalInfoDao.save(pi));
+				}
+			}
+		}catch(Exception ex) {
+			responseEntity = new ResponseEntity<>(new ErrorMessage(new Date(), ex), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		
+		return responseEntity;
+	}
 
 }
